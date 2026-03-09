200fea9fb58d83e1e3aed2d54da7e3c5fea761b0
Racuun <lukaszkmiecikdodtkowe@gmail.com>
2026-03-09 17:07:03 +0100

    chore: add sub function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+int sub(int a, int b) {
+    return a - b;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    
+    // test the sub function
+    std::cout << "5 - 3 = " << sub(5, 3) << std::endl;
     return 0;
 }
