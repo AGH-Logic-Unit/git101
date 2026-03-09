e12c01f74fe19d7bed39fbaceb27ae32da68a963
Racuun <lukaszkmiecikdodtkowe@gmail.com>
2026-03-09 17:08:48 +0100

    chore: add multiply function

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+int multiply(int a, int b) {
+    return a * b;
+}
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    // test the multiply function
+    std::cout << "2 * 3 is: " << multiply(2, 3) << std::endl;
     return 0;
 }
